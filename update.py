01444137e3ebbef0b9815f1b5dfc9891b084fc59
Jackmick99 <jacopomicheletto@gmail.com>
2026-09-12 18:58:13 +0200

    Change YouTube URL and add cookie file cleanup
    Updated YouTube video URL and ensured cookie file cleanup.

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 from pathlib import Path
 
-URL = "https://www.youtube.com/watch?v=wHJHpOP7vjM"
+URL = "https://www.youtube.com/watch?v=_nd_bpGoMVE"
 OUTPUT = Path("trm.m3u8")
 
 cookies = os.environ.get("YOUTUBE_COOKIES")
